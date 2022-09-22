b1e3c064ba5d762ae539da25cafa7d84c0d2a6f8
trungvi123 <trungvi123@gmail.com>
2022-09-22 21:20:14 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
   return next(new ApiError(404, "Resource not found"));
 });
 
-app.use((err, req, res, next) => {
+app.use((error, req, res, next) => {
   return res.status(error.statusCode || 500).json({
     message: error.message || "Internal Server Error",
   });
